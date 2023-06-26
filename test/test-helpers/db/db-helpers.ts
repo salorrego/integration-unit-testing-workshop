b8016a50ce88e4ddb2ff893be7087362624ca472
Salomon Orrego <salorrego@gmail.com>
2023-06-26 17:53:16 -0500

    feat: prject setup

--- test/test-helpers/db/db-helpers.ts
+++ test/test-helpers/db/db-helpers.ts
@@ -15,6 +15,64 @@ function createClient(): Client {
   });
 }
 
+/**
+ * @return {Promise<QueryResult<any>>} Return all migrations
+ */
+export async function getMigrations(): Promise<QueryResult<any>> {
+  const client = createClient();
+  await client.connect();
+  const res = await client.query('SELECT * FROM "migrations";');
+  client.end();
+
+  return res;
+}
+
+/**
+ * @return {Promise<void>} Truncate a table
+ */
+export async function truncateTable(table: string): Promise<void> {
+  const client = createClient();
+  await client.connect();
+  client.query(`TRUNCATE "${table}" CASCADE;`, async (err) => {
+    if (err) {
+      console.log(err);
+    }
+    await client.end();
+  });
+}
+
+/**
+ * Timeout helper function
+ */
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * @return {Promise<void>} Wait for postgres to be ready to accept connections
+ */
+export async function waitForPostgres(
+  DEFAULT_MAX_ATTEMPTS = 20,
+  DEFAULT_DELAY = 150,
+): Promise<void> {
+  let didConnect = false;
+  let retries = 0;
+  while (!didConnect) {
+    try {
+      const client = createClient();
+      await client.connect();
+      console.log('✅ Postgres is ready to accept connections');
+      client.end();
+      didConnect = true;
+    } catch (error) {
+      retries += 1;
+      if (retries > DEFAULT_MAX_ATTEMPTS) {
+        throw error;
+      }
+      console.log('😴 Postgres is unavailable - sleeping');
+      await timeout(DEFAULT_DELAY);
+    }
+  }
+}
+
 export async function saveBook({
   name,
   author,
@@ -24,7 +82,7 @@ export async function saveBook({
 }): Promise<QueryResult<any>> {
   let client;
   try {
-    const query = `INSERT INTO "books" ("name", "author", "genre", "quantity", "totalAvailable") 
+    const query = `INSERT INTO "books" ("name", "author", "genre", "quantity", "totalAvailable")
     VALUES ($1, $2, $3, $4, $5)`;
     client = createClient();
     await client.connect();
